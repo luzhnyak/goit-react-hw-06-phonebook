7a1c945735ce66302e90ad6e6404755fe030404b
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-10-12 00:25:45 +0300

    finish

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,10 +5,6 @@ const contactsInitialState = [
   { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
   { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
   { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
 ];
 
 const contactsSlice = createSlice({
